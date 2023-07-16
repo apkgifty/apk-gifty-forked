85743d2fff1d8e127e75be4cda01eb41ac33a7f4
huntdavid175 <huntdavid175@gmail.com>
2023-07-16 14:32:31 +0000

    code clean up

---
+++ components/Error/ErrorWrapper.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const ErrorWrapper = ({ children }: { children: React.ReactNode }) => {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    const handleOnline = () => {
+      //   console.log(navigator.onLine);
+      setIsOnline(true);
+    };
+    const handleOffline = () => {
+      setIsOnline(false);
+      console.log("offline");
+    };
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+  return <>{isOnline ? children : <p>Oops you're offline</p>}</>;
+};
+
+export default ErrorWrapper;
