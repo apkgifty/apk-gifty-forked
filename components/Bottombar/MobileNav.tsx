bb350a291572008ddc59f22f0db9da840ce34716
huntdavid175 <huntdavid175@gmail.com>
2023-07-17 15:30:04 +0000

    scale aniimation added to mobile navigation

--- components/Bottombar/MobileNav.tsx
+++ components/Bottombar/MobileNav.tsx
@@ -4,26 +4,28 @@ import ExchangeSvg from "../UI/SvgIcons/ExchangeSvg";
 import WalletSvg from "../UI/SvgIcons/WalletSvg";
 import TransactionSvg from "../UI/SvgIcons/TransactionSvg";
 import NewsSvg from "../UI/SvgIcons/NewsSvg";
+import { motion } from "framer-motion";
+import ScaleAnimate from "../Animations/ScaleAnimate";
 
 const MobileNav = () => {
   return (
-    <div className="w-full flex justify-center text-white absolute bottom-16 mb-8 lg:hidden">
+    <div className="w-full flex justify-center text-white absolute bottom-20 mb-8 lg:hidden">
       <div className="w-[80%] flex justify-around bg-[#587BF2] px-4 py-3 rounded-xl z-100">
-        <div>
+        <ScaleAnimate>
           <DashboardSvg />
-        </div>
-        <div>
+        </ScaleAnimate>
+        <ScaleAnimate>
           <ExchangeSvg />
-        </div>
-        <div>
+        </ScaleAnimate>
+        <ScaleAnimate>
           <WalletSvg />
-        </div>
-        <div>
+        </ScaleAnimate>
+        <ScaleAnimate>
           <TransactionSvg />
-        </div>
-        <div>
+        </ScaleAnimate>
+        <ScaleAnimate>
           <NewsSvg />
-        </div>
+        </ScaleAnimate>
       </div>
     </div>
   );
