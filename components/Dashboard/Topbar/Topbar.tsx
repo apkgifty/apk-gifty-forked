ca2546d0c78223f3625d737ceff29b205686d139
huntdavid175 <huntdavid175@gmail.com>
2023-07-25 11:36:47 +0000

    test build

--- components/Dashboard/Topbar/Topbar.tsx
+++ components/Dashboard/Topbar/Topbar.tsx
@@ -6,7 +6,7 @@ import WalletSvg from "@/components/UI/SvgIcons/WalletSvg";
 
 const Topbar = () => {
   return (
-    <div className="w-full px-6 py-4 fixed top-0 z-10 flex justify-between bg-secondary border-b border-tertiary text-white">
+    <div className="w-full px-6 py-4 sticky top-0 z-10 flex justify-between bg-secondary border-b border-tertiary text-white">
       <div></div>
       <div className="flex gap-x-2">
         <LanguageSelect />
