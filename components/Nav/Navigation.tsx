7f833da0ab541e6683fe0cb2c4c2516cf41378c1
huntdavid175 <huntdavid175@gmail.com>
2023-08-07 22:32:28 +0000

    btn fix test

--- components/Nav/Navigation.tsx
+++ components/Nav/Navigation.tsx
@@ -37,17 +37,16 @@ const Navigation = () => {
       </div>
       <div className="hidden lg:flex lg:gap-x-3">
         <div className="space-x-1">
-          {/* <Link href={"/login"}> */}
-          {/* <button className="bg-tertiary px-3 py-2 text-sm font-medium rounded-lg">
-            Login
-          </button> */}
-          {/* </Link> */}
-          {/* <Link href={"/signup"}> */}
-
-          {/* <button className="bg-appviolet px-3 py-2 text-sm font-medium rounded-lg">
-            Signup
-          </button> */}
-          {/* </Link> */}
+          <Link href={"/login"}>
+            <button className="bg-tertiary px-3 py-2 text-sm font-medium rounded-lg">
+              Login
+            </button>
+          </Link>
+          <Link href={"/signup"}>
+            <button className="bg-appviolet px-3 py-2 text-sm font-medium rounded-lg">
+              Signup
+            </button>
+          </Link>
         </div>
         {/* <LanguageSelect /> */}
       </div>
