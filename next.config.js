404b26596289400b7140bc0e1bc4b716af6e02f7
huntdavid175 <huntdavid175@gmail.com>
2023-08-08 22:53:35 +0000

    added image src to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "placehold.co"],
+    domains: ["images.unsplash.com", "placehold.co", "backend.apkxchange.com"],
     dangerouslyAllowSVG: true,
   },
   experimental: {
