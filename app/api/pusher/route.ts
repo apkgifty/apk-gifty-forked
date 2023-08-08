b99ae0f333389624e1fbf3532abfea1dd5992642
huntdavid175 <huntdavid175@gmail.com>
2023-08-08 13:26:52 +0000

    pusher error fixed

--- app/api/pusher/route.ts
+++ app/api/pusher/route.ts
@@ -3,15 +3,14 @@ import Pusher from "pusher";
 import axiosInstance from "@/utils/axios";
 import { NextResponse } from "next/server";
 
-export const pusher = new Pusher({
-  appId: "1630971",
-  key: "e597b63b0a16d6c4a2c6",
-  secret: "9213167c7ce619f23cb5",
-  cluster: "mt1",
-  useTLS: true,
-});
-
 export async function POST(req: Request, res: Response) {
+  const pusher = new Pusher({
+    appId: "1630971",
+    key: "e597b63b0a16d6c4a2c6",
+    secret: "9213167c7ce619f23cb5",
+    cluster: "mt1",
+    useTLS: true,
+  });
   const { message, sender } = await req.json();
 
   console.log(message, sender);
