11a89ae113cb482892ced18805c1ea66cb088fdc
huntdavid175 <huntdavid175@gmail.com>
2023-08-09 11:18:14 +0000

    provider added to auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import "../globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+import { Providers } from "@/redux/provider";
 import Navbar from "@/components/Nav/Navbar";
 import ImagesCard from "@/components/UI/ImagesCard";
 import AuthLayoutText from "@/components/Card/CardTexts/AuthLayoutText";
@@ -20,22 +21,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-tertiary`}>
-        <div className="w-full h-screen flex flex-col">
-          <Navbar />
-          <div className="w-full bg-tertiary flex  flex-col flex-1 justify-center items-center text-white">
-            <div className="w-full lg:max-w-5xl">
-              <FormCard>
-                <div className="w-full flex gap-2 ">
-                  <div className="flex-1 ">{children}</div>
-                  <div className="flex-1 bg-transparent hidden  justify-center items-center lg:flex lg:flex-col">
-                    <ImagesCard />
-                    <AuthLayoutText />
+        <Providers>
+          <div className="w-full h-screen flex flex-col">
+            <Navbar />
+            <div className="w-full bg-tertiary flex  flex-col flex-1 justify-center items-center text-white">
+              <div className="w-full lg:max-w-5xl">
+                <FormCard>
+                  <div className="w-full flex gap-2 ">
+                    <div className="flex-1 ">{children}</div>
+                    <div className="flex-1 bg-transparent hidden  justify-center items-center lg:flex lg:flex-col">
+                      <ImagesCard />
+                      <AuthLayoutText />
+                    </div>
                   </div>
-                </div>
-              </FormCard>
+                </FormCard>
+              </div>
             </div>
           </div>
-        </div>
+        </Providers>
       </body>
     </html>
   );
