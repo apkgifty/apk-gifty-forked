58c6adb2d21463b10856bd6315df9c19de3412f7
huntdavid175 <huntdavid175@gmail.com>
2023-08-10 18:13:48 +0000

    pusher commented

--- app/api/pusher/route.ts
+++ app/api/pusher/route.ts
@@ -4,13 +4,13 @@ import axiosInstance from "@/utils/axios";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request, res: Response) {
-  const pusher = new Pusher({
-    appId: "1630971",
-    key: "e597b63b0a16d6c4a2c6",
-    secret: "9213167c7ce619f23cb5",
-    cluster: "mt1",
-    useTLS: true,
-  });
+  // const pusher = new Pusher({
+  //   appId: "1630971",
+  //   key: "e597b63b0a16d6c4a2c6",
+  //   secret: "9213167c7ce619f23cb5",
+  //   cluster: "mt1",
+  //   useTLS: true,
+  // });
   const { message, sender, userId } = await req.json();
 
   console.log(message, sender);
