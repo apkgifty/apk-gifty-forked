58d4f16451f05a165c906c9ac4b253fc9d655c52
huntdavid175 <huntdavid175@gmail.com>
2023-08-22 16:55:08 +0000

    main navigation links fixed

--- components/Nav/NavigationItem.tsx
+++ components/Nav/NavigationItem.tsx
@@ -3,14 +3,21 @@
 import { MainNav } from "@/types/navTypes";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import path from "path";
+import { useState } from "react";
 
 const NavigationItem: React.FC<MainNav> = ({ title, link }) => {
   const pathName = usePathname();
+
   return (
     <Link
       href={link}
       className={`${
-        pathName.includes(link) ? "text-violet-500" : "text-white"
+        pathName === "/" && title.toLowerCase() === "home"
+          ? "text-violet-500"
+          : pathName.includes(title.toLowerCase())
+          ? "text-violet-500"
+          : "text-white"
       } hover:underline`}
     >
       {title}
