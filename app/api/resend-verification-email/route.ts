0a5615d9f68827b13d80d12a55eaa9bd44724a76
huntdavid175 <huntdavid175@gmail.com>
2023-11-08 16:05:00 +0000

    testing deployment bug fix

--- app/api/resend-verification-email/route.ts
+++ app/api/resend-verification-email/route.ts
@@ -1,8 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axiosInstance from "@/utils/axios";
 import { NextResponse } from "next/server";
+import axios from "axios";
+import { cookies } from "next/headers";
 
 export async function GET(req: Request, res: Response) {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("access")?.value;
   //   const body = await req.json();
   //   console.log(body);
 
@@ -24,8 +28,9 @@ export async function GET(req: Request, res: Response) {
   };
 
   try {
-    // const response = await axiosInstance(config);
-    // return NextResponse.json(response.data);
+    const response = await axios(config);
+
+    return NextResponse.json(response.data);
   } catch (error: any) {
     return new Response(JSON.stringify(error.response.data), {
       status: error.response.status,
