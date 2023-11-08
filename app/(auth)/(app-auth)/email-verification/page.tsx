963f2852d9a6cac69e8db234e96606e63ec04377
huntdavid175 <huntdavid175@gmail.com>
2023-11-08 15:15:11 +0000

    testing deployment bug fix

--- app/(auth)/(app-auth)/email-verification/page.tsx
+++ app/(auth)/(app-auth)/email-verification/page.tsx
@@ -1,38 +1,38 @@
-import { redirect } from "next/navigation";
-import axiosInstance from "@/utils/axios";
+// import { redirect } from "next/navigation";
+// import axiosInstance from "@/utils/axios";
 
-import EmailVerificationForm from "@/components/Form/EmailVerificationForm";
+// import EmailVerificationForm from "@/components/Form/EmailVerificationForm";
 
-const EmailVerificationPage = async () => {
-  let userEmailVerificationStatus: any = null;
-  try {
-    const config = {
-      method: "GET",
-      url: "/profile",
-      maxBodyLength: Infinity,
-      headers: {
-        "Content-Type": "application/json",
-        accept: "application/json",
-      },
-    };
-    const response = await axiosInstance(config);
-    // console.log(response.data);
-    userEmailVerificationStatus = response.data.data.email_verified_at;
-  } catch (error) {
-    console.log(error);
-  }
+// const EmailVerificationPage = async () => {
+//   let userEmailVerificationStatus: any = null;
+//   try {
+//     const config = {
+//       method: "GET",
+//       url: "/profile",
+//       maxBodyLength: Infinity,
+//       headers: {
+//         "Content-Type": "application/json",
+//         accept: "application/json",
+//       },
+//     };
+//     const response = await axiosInstance(config);
+//     // console.log(response.data);
+//     userEmailVerificationStatus = response.data.data.email_verified_at;
+//   } catch (error) {
+//     console.log(error);
+//   }
 
-  // console.log(userEmailVerificationStatus);
+//   // console.log(userEmailVerificationStatus);
 
-  if (userEmailVerificationStatus !== null) {
-    redirect("/dashboard/exchange/buy");
-  }
+//   if (userEmailVerificationStatus !== null) {
+//     redirect("/dashboard/exchange/buy");
+//   }
 
-  return (
-    <div>
-      <EmailVerificationForm />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <EmailVerificationForm />
+//     </div>
+//   );
+// };
 
-export default EmailVerificationPage;
+// export default EmailVerificationPage;
