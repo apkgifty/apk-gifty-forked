f69345a0c69a7e2aa3acc01a0932a16bb4338870
huntdavid175 <huntdavid175@gmail.com>
2024-07-04 14:21:27 -0400

    Created the faqs page

--- app/(dashboard)/dashboard/faq/page.tsx
+++ app/(dashboard)/dashboard/faq/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const FaqsPage = () => {
   return (
-    <div className="w-full flex flex-wrap gap-x-12 gap-y-12 px-4 justify-center mx-auto mt-8 xl:max-w-[1700px]">
+    <div className="w-full flex flex-wrap gap-x-12 gap-y-12 px-4 xl:px-24 justify-center mx-auto mt-8  xl:max-w-[1300px]">
       <FaqAccordion />
     </div>
   );
