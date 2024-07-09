13168b868f08a0d89e2d9d366a82bfc9c78ed335
huntdavid175 <huntdavid175@gmail.com>
2024-07-09 19:30:00 -0400

    changed signup endpoint to test

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request, res: Response) {
   let config = {
     method: "POST",
     maxBodyLength: Infinity,
-    url: "https://backend.apkxchange.com/api/register",
+    url: "https://register.apkxchange.com/api/register",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
