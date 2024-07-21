9651ad9cf98d39ad75c636dbc5ebf009c5f0da97
huntdavid175 <huntdavid175@gmail.com>
2024-07-21 13:30:07 -0400

    commented other-services slug page

--- app/(dashboard)/dashboard/other-services/[slug]/page.tsx
+++ app/(dashboard)/dashboard/other-services/[slug]/page.tsx
@@ -1,9 +1,9 @@
-// import { redirect } from "next/navigation";
-// import React from "react";
-// import axios from "axios";
-// import { cookies } from "next/headers";
-// import BankCard from "@/components/Card/BankCards/BankCard";
-// import Product from "@/components/Product/Product";
+import { redirect } from "next/navigation";
+import React from "react";
+import axios from "axios";
+import { cookies } from "next/headers";
+import BankCard from "@/components/Card/BankCards/BankCard";
+import Product from "@/components/Product/Product";
 
 // const fetchProducts = async (accessToken: any, type: string) => {
 //   const response = await axios.get(
@@ -18,43 +18,43 @@
 //   return response.data.data;
 // };
 
-// const OtherServicesAllPage = async ({
-//   params,
-// }: {
-//   params: { slug: string };
-// }) => {
-//   if (params.slug !== "Bank" && params.slug !== "Bundle") {
-//     redirect("/404");
-//   }
+const OtherServicesAllPage = async ({
+  params,
+}: {
+  params: { slug: string };
+}) => {
+  // if (params.slug !== "Bank" && params.slug !== "Bundle") {
+  //   redirect("/404");
+  // }
 
-//   const cookieStore = cookies();
-//   const accessToken = cookieStore.get("accessToken");
+  // const cookieStore = cookies();
+  // const accessToken = cookieStore.get("accessToken");
 
-//   const data = await fetchProducts(accessToken, params.slug);
+  // const data = await fetchProducts(accessToken, params.slug);
 
-//   console.log(data);
+  // console.log(data);
 
-//   let lists = null;
-//   let title = null;
+  // let lists = null;
+  // let title = null;
 
-//   if (params.slug === "Bank") {
-//     title = "All Banks";
-//     lists = data.map((item: any) => <BankCard key={item.id} />);
-//   } else if (params.slug === "Bundle") {
-//     title = "All Data Bundles";
-//     lists = data.map((item: any) => (
-//       <Product key={item.id} productInfo={item} />
-//     ));
-//   }
+  // if (params.slug === "Bank") {
+  //   title = "All Banks";
+  //   lists = data.map((item: any) => <BankCard key={item.id} />);
+  // } else if (params.slug === "Bundle") {
+  //   title = "All Data Bundles";
+  //   lists = data.map((item: any) => (
+  //     <Product key={item.id} productInfo={item} />
+  //   ));
+  // }
 
-//   return (
-//     <div className="w-full flex flex-wrap gap-x-12 gap-y-12 px-4 justify-center mx-auto mt-8 xl:max-w-[1700px]">
-//       <div className="w-full flex justify-center">
-//         <h4 className="text-white text-xl lg:text:3xl">{title}</h4>
-//       </div>
-//       {lists}
-//     </div>
-//   );
-// };
+  return (
+    <div className="w-full flex flex-wrap gap-x-12 gap-y-12 px-4 justify-center mx-auto mt-8 xl:max-w-[1700px]">
+      {/* <div className="w-full flex justify-center">
+        <h4 className="text-white text-xl lg:text:3xl">{title}</h4>
+      </div>
+      {lists} */}
+    </div>
+  );
+};
 
-// export default OtherServicesAllPage;
+export default OtherServicesAllPage;
