bc3c51eb3e235a7797b223bf848933281b51e384
huntdavid175 <huntdavid175@gmail.com>
2024-07-23 22:22:30 -0400

    forgot password sending password link to email implemented

--- components/Form/ForgotPasswordForm.tsx
+++ components/Form/ForgotPasswordForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import FormHeader from "./FormComponents/FormHeader";
 import FormBody from "./FormComponents/FormBody";
 import FormContainer from "./FormComponents/FormContainer";
@@ -9,32 +10,31 @@ import { motion } from "framer-motion";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import { useRouter } from "next/navigation";
 
+const regex: RegExp =
+  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
+
 const fields: Fields[] = [
   {
-    type: "text",
+    type: "email",
     placeholder: "Email",
-    name: "email",
     icon: <AlternateEmailIcon />,
     config: {
-      required: true,
+      required: { value: true, message: "Email is required" },
+      pattern: { value: regex, message: "Invalid email address" },
     },
+    name: "email",
   },
 ];
 
 const ForgotPasswordForm = () => {
   const router = useRouter();
 
+  const [emailSent, setEmailSent] = useState(false);
+
   const afterSubmit = (data: any) => {
-    if (data?.token) {
-      // console.log(data);
-      // console.log(data.token);
-      // const expiresInSeconds = 3 * 60 * 60;
-      // setCookie("access", data.token, { maxAge: expiresInSeconds });
-      // if (data.user.email_verified_at !== null) {
-      //   router.push("/dashboard/exchange/buy");
-      // } else {
-      //   router.push("/email-verification");
-      // }
+    console.log(data);
+    if (data?.status) {
+      setEmailSent(true);
     }
   };
   return (
@@ -52,12 +52,18 @@ const ForgotPasswordForm = () => {
         >
           <img src="/watch-it.svg" />
         </motion.div>
-        <Form
-          fields={fields}
-          redirectUrl="/"
-          endpoint="#"
-          afterSubmit={() => {}}
-        />
+        {!emailSent ? (
+          <Form
+            fields={fields}
+            redirectUrl="/"
+            endpoint="/api/forgot-password"
+            afterSubmit={afterSubmit}
+          />
+        ) : (
+          <p className="text-white text-xs lg:text-sm text-center">
+            A password reset link has been sent to your email address
+          </p>
+        )}
       </FormBody>
     </FormContainer>
   );
