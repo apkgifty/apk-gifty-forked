397cfd140f735ead937434bd122556bc9efbe41f
huntdavid175 <huntdavid175@gmail.com>
2024-08-02 18:28:10 -0400

    removed logs

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request, res: Response) {
   const body = await req.json();
 
-  console.log(body);
+  // console.log(body);
   let data = JSON.stringify({
     ...body,
   });
