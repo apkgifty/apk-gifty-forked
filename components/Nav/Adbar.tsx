429341c8f6e2e2ceb2d4447de7a99b283f4d904a
huntdavid175 <huntdavid175@gmail.com>
2024-08-17 21:30:13 -0400

    added animation to adbar text

--- components/Nav/Adbar.tsx
+++ components/Nav/Adbar.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import React from "react";
 import DownloadApp from "../Main/Section/DownloadApp";
+import { motion } from "framer-motion";
 
 const Adbar = () => {
   return (
     <div className="w-full py-2 flex justify-center items-center lg:space-x-8 px-8">
-      <p className="text-white font-light text-sm lg:text-lg">
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="text-white font-light text-sm lg:text-lg"
+      >
         Trading made easy
-      </p>
+      </motion.p>
       <DownloadApp />
     </div>
   );
