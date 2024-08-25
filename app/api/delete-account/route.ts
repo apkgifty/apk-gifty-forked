f6699a96f55ddc1c45f357f418b622c20772e51c
huntdavid175 <huntdavid175@gmail.com>
2024-08-25 17:00:30 -0400

    added delete api endpoint

---
+++ app/api/delete-account/route.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import axios from "axios";
+import axiosInstance from "@/utils/axios";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request, res: Response) {
+  let config = {
+    method: "DELETE",
+    maxBodyLength: Infinity,
+    url: `${process.env.API_ENDPOINT}/account`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const response = await axiosInstance(config);
+    console.log(response);
+    return NextResponse.json(response.data);
+  } catch (error: any) {
+    return new Response(JSON.stringify(error.response.data), {
+      status: error.response.status,
+      headers: error.response.header,
+    });
+  }
+}
